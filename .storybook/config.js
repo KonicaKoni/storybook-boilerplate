e357694292cc56599174af82bd1016f8757652e1
unknown <konrad.leon.toldy@gmail.com>
2018-09-12 10:42:41 +0200

    config promjena

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,14 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../stories/button.js');
+  require('../stories/card');
+  require('../stories/footer');
+  require('../stories/header');
+  require('../stories/main-content');
+  require('../stories/navigation-menu');
+  require('../stories/roundicon-button');
+  require('../stories/popup');
+  require('../stories/table.js');
 }
 
 configure(loadStories, module);
